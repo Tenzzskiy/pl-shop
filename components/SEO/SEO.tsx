8936a591ee5023245e3163c6ebd3093f34472fa7
tenzzskit <tenzzskiy@mail.ru>
2022-02-02 15:41:12 +0300

    13 day - make modals for contacts

--- components/SEO/SEO.tsx
+++ components/SEO/SEO.tsx
@@ -1,6 +1,7 @@
 import styles from './SEO.module.scss'
-import React, {FunctionComponent, ReactNode} from "react";
+import React, {FunctionComponent, ReactNode, useState} from "react";
 import {useWindowSize} from "../../Hooks/useWindowSize";
+import cs from 'classnames'
 
 interface SeoProps{
     children?:ReactNode,
@@ -14,6 +15,10 @@ interface SeoProps{
 }
 const Seo:FunctionComponent = ({children, image1,image2,text1,text2,text3,text4,otherText}:SeoProps) =>{
     const size=useWindowSize();
+    const [active,setActive]= useState(true);
+    const text = `${text1}${text2}${text3}${text4} `
+    const step = 210;
+    const items = text.slice(0,active ? step : text.length)
     return(
         <div className={styles.seo}>
         <div className={styles.container}>
@@ -24,60 +29,96 @@ const Seo:FunctionComponent = ({children, image1,image2,text1,text2,text3,text4,
                 </div>
             <div className={styles.content}>
             <div className={styles.grid}>
-                    <div className={styles.box1}><img src={image1} alt=""/>
-                        {size.width < 1100 && size.width > 600 ?
-                            <img src="/Seo/background_768.svg" className={styles.background_image} alt=""/> :null
-                        }
+                {/*    <div className={styles.box1}><img src={image1} alt=""/>*/}
+                {/*        {size.width < 1100 && size.width > 600 ?*/}
+                {/*            <img src="/Seo/background_768.svg" className={styles.background_image} alt=""/> :null*/}
+                {/*        }*/}
 
-                        {size.width > 1100 ?
-                            <>
-                                <img src="/Seo/background_image.svg" className={styles.background_image} alt=""/>
-                            </>
-                            :
-                            null
+                {/*        {size.width > 1100 ?*/}
+                {/*            <>*/}
+                {/*                <img src="/Seo/background_image.svg" className={styles.background_image} alt=""/>*/}
+                {/*            </>*/}
+                {/*            :*/}
+                {/*            null*/}
 
-                        }
+                {/*        }*/}
 
-                    </div>
-                    <div className={styles.box2}>{text1}</div>
-                    <div className={styles.box3}>{text2} </div>
-                <div className={styles.box4}><img src={image2} alt=""/>
+                {/*    </div>*/}
+                {/*    <div className={styles.box2}>{text1}</div>*/}
+                {/*    <div className={styles.box3}>{text2} </div>*/}
+                {/*<div className={styles.box4}><img src={image2} alt=""/>*/}
 
-                    {size.width < 1100 && size.width > 600 ?
-                        <img src="/Seo/background_768.svg" className={styles.background_image} alt=""/> :null
-                    }
+                {/*    {size.width < 1100 && size.width > 600 ?*/}
+                {/*        <img src="/Seo/background_768.svg" className={styles.background_image} alt=""/> :null*/}
+                {/*    }*/}
 
-                    {size.width > 1100 ?
-                        <>
-                            <img src="/Seo/background_image.svg" className={styles.background_image} alt=""/>
-                        </>
-                        :
-                        null
+                {/*    {size.width > 1100 ?*/}
+                {/*        <>*/}
+                {/*            <img src="/Seo/background_image.svg" className={styles.background_image} alt=""/>*/}
+                {/*        </>*/}
+                {/*        :*/}
+                {/*        null*/}
 
-                    }
+                {/*    }*/}
 
+                {/*</div>*/}
+                {/*{size.width > 1100 ?*/}
+                {/*    <>*/}
+                {/*        <div className={styles.box5}>{text3}{otherText}</div>*/}
+                {/*        <div className={styles.box6}>{text4}</div>*/}
+                {/*    </>*/}
+                {/*    :*/}
+                {/*    null*/}
+
+                {/*}*/}
+
+                {/*{size.width < 1100 && size.width > 600 ?*/}
+                {/*    <>*/}
+                {/*        <div className={styles.box5}>{text3}</div>*/}
+                {/*        <div className={styles.box6}>{otherText}{text4}</div>*/}
+                {/*    </>*/}
+                {/*   :*/}
+                {/*    null*/}
+
+                {/*}*/}
+
+                {size.width > 720 ?
+                   <>
+                       <div className={styles.container_1}>
+                           <div className={styles.img}>
+                               <img src="/Seo/background_768.svg" className={styles.background_image} alt=""/>
+                               <img src={image1} className={styles.image} alt=""/>
+                           </div>
+                           <div className={styles.text}>{text1}{text2}
+                           </div>
+                       </div>
+                       <div className={styles.container_2}>
+                           <div className={styles.img}>
+                               <img src="/Seo/background_768.svg" className={styles.background_image2} alt=""/>
+                               <img src={image2} className={styles.image_2} alt=""/>
+                           </div>
+                           <div className={styles.text}>{text3}{text4}
+                           </div>
+                       </div>
+                   </> : null
+                }
+                {size.width < 720 ?
+
+                <>
+                <div className={styles.container_360}>
+                <div className={styles.img_360}>
+                    <img src="/Seo/background_768.svg" className={styles.background_image_360} alt=""/>
+                    <img src={image1} className={styles.image_360} alt=""/>
                 </div>
-                {size.width > 1100 ?
-                    <>
-                        <div className={styles.box5}>{text3}{otherText}</div>
-                        <div className={styles.box6}>{text4}</div>
-                    </>
-                    :
-                    null
-
-                }
-
-                {size.width < 1100 && size.width > 600 ?
-                    <>
-                        <div className={styles.box5}>{text3}</div>
-                        <div className={styles.box6}>{otherText}{text4}</div>
-                    </>
-                   :
-                    null
-
-                }
-
-
+                    <div className={cs(styles.text_360,active ? styles.show : styles.hide)}>
+                        {items}{active ?  '...' :  null}
+                    </div>
+                    <div className={styles.button} onClick={() => setActive(!active)}> {active ?  'Подробнее':'Скрыть' }
+                        <img src={active ? "/Seo/arrow.svg" : "/Seo/arrow_top.svg"} alt=""/>
+                    </div>
+                </div>
+                </>
+                : null}
 
             </div>
             </div>
--- pages/screens.js
+++ pages/screens.js
@@ -1,4 +1,3 @@
-
 import Layout from "../components/Layout/Layout";
 import {Conditions} from "../components/Conditions/Conditions";
 import {Preferences} from "../components/Preferences/Preferences";
@@ -24,7 +23,7 @@ import data from "../components/Shop/cart_arenda-plasm77.ru.json";
 import {OfferModal} from "../components/offerModal/OfferModal";
 import SitesModal from "../components/SitesModal/SitesModal";
 
-export  const getStaticProps = async () =>{
+export const getStaticProps = async () => {
     return {
         props: {
             cards: data
@@ -32,145 +31,119 @@ export  const getStaticProps = async () =>{
     };
 }
 
-const Screens = ({  cards})=> {
+const Screens = ({cards}) => {
     const size = useWindowSize();
-    const [modalActive,setModalActive] = useState(false)
-    const [offerModal,setOfferModal] = useState(false)
-    const [sites,setSites] = useState(false);
+    const [modalActive, setModalActive] = useState(false)
+    const [offerModal, setOfferModal] = useState(false)
+    const [sites, setSites] = useState(false);
     return (
-      <Provider store={store}>
-          <Layout >
-              <HeaderComponent
-                  title='Аренда  светодиодных экранов'
-                  mainImg='/header/Screen/main.jpg'
-                  button_text='Рассчитать стоимость'
-                  purple={true}
-                  setSites={setSites}
-                  setOfferModal={setOfferModal}
-              />
-              {size.width > 1200 ?
-                  <Preferences
-                      count={1}
-                      leftImg='/Preferences/Screen/1.jpg'
-                      mainTitle=' Преимущества светодиодных экранов'
-                      leftTitle='Зрелищность '
-                      leftText='Выгодно выделяют ваш стенд, презентацию или сцену,
+        <Provider store={store}>
+            <Layout>
+                <HeaderComponent
+                    title='Аренда  светодиодных экранов'
+                    mainImg='/header/Screen/main.jpg'
+                    button_text='Рассчитать стоимость'
+                    purple={true}
+                    setSites={setSites}
+                    setOfferModal={setOfferModal}
+                />
+                {size.width > 1200 ?
+                    <Preferences
+                        count={1}
+                        leftImg='/Preferences/Screen/1.jpg'
+                        mainTitle=' Преимущества светодиодных экранов'
+                        leftTitle='Зрелищность '
+                        leftText='Выгодно выделяют ваш стенд, презентацию или сцену,
                          а также зрелищно организуют пространство в вашем заведении! '
-                  /> : null
+                    /> : null
 
-              }
-              {size.width < 1200 && size.width > 720 ?
-                  <Preferences_768
-                      count={1}
-                      leftImg='/Preferences/Screen/1.jpg'
-                      title=' Преимущества светодиодных экранов'
-                      leftTitle='Зрелищность '
-                      leftText='Выгодно выделяют ваш стенд, презентацию или сцену,
+                }
+                {size.width < 1200 && size.width > 720 ?
+                    <Preferences_768
+                        count={1}
+                        leftImg='/Preferences/Screen/1.jpg'
+                        title=' Преимущества светодиодных экранов'
+                        leftTitle='Зрелищность '
+                        leftText='Выгодно выделяют ваш стенд, презентацию или сцену,
                          а также зрелищно организуют пространство в вашем заведении! '
-                  /> : null
-              }
-              {
-                  size.width <720 ?
-                      <Preferences_360
-                          count={1}
-                          leftImg='/Preferences/Screen/1.jpg'
-                          mainTitle=' Преимущества светодиодных экранов'
-                          leftTitle='Зрелищность '
-                          leftText='Выгодно выделяют ваш стенд, презентацию или сцену,
+                    /> : null
+                }
+                {
+                    size.width < 720 ?
+                        <Preferences_360
+                            count={1}
+                            leftImg='/Preferences/Screen/1.jpg'
+                            mainTitle=' Преимущества светодиодных экранов'
+                            leftTitle='Зрелищность '
+                            leftText='Выгодно выделяют ваш стенд, презентацию или сцену,
                          а также зрелищно организуют пространство в вашем заведении! '
-                      /> : null
-              }
+                        /> : null
+                }
 
-              <Conditions />
-              <Shop page={3} active={modalActive} setActive={setModalActive} data={cards}/>
-              {size.width > 1200 ? <Offer active={modalActive} setActive={setModalActive}/> : null }
-              {size.width > 720 && size.width<1200 ? <Offer_768  active={modalActive} setActive={setModalActive}/> : null }
-              {size.width < 720  ? <Offer_360 active={modalActive} setActive={setModalActive}
-              /> : null }
-              <Cleaning />
-              <Sale />
-              {size.width > 1200 ?
-                  <Releases
-                      img1='/releases/Screen/1.jpg'
-                      img2='/releases/Screen/2.jpg'
-                      img3='/releases/Screen/3.jpg'
-                      img4='/releases/Screen/4.jpg'
-                      img5='/releases/Screen/5.jpg'
-                      text1='Угловая LED фотозона'
-                      text2='Брендированный экспостенд'
-                      text3='LED экраны в оформлении сцены для презентации нового продукта'
-                      text4='Светодиодный экран для оформления сцены'
-                      text5='Светодиодный экран для оформления фотозоны'
-                  /> : null
-              }
-              { size.width < 1200  ?
-                  <Releases_768
-                      img1='/releases/Screen/1.jpg'
-                      img2='/releases/Screen/2.jpg'
-                      img3='/releases/Screen/3.jpg'
-                      img4='/releases/Screen/4.jpg'
-                      img5='/releases/Screen/5.jpg'
-                      text1='Угловая LED фотозона'
-                      text2='Брендированный экспостенд'
-                      text3='LED экраны в оформлении сцены для презентации нового продукта'
-                      text4='Светодиодный экран для оформления сцены'
-                      text5='Светодиодный экран для оформления фотозоны'
-                  /> : null
-              }
+                <Conditions/>
+                <Shop page={3} active={modalActive} setActive={setModalActive} data={cards}/>
+                {size.width > 1200 ? <Offer active={modalActive} setActive={setModalActive}/> : null}
+                {size.width > 720 && size.width < 1200 ?
+                    <Offer_768 active={modalActive} setActive={setModalActive}/> : null}
+                {size.width < 720 ? <Offer_360 active={modalActive} setActive={setModalActive}
+                /> : null}
+                <Cleaning/>
+                <Sale/>
+                {size.width > 1200 ?
+                    <Releases
+                        img1='/releases/Screen/1.jpg'
+                        img2='/releases/Screen/2.jpg'
+                        img3='/releases/Screen/3.jpg'
+                        img4='/releases/Screen/4.jpg'
+                        img5='/releases/Screen/5.jpg'
+                        text1='Угловая LED фотозона'
+                        text2='Брендированный экспостенд'
+                        text3='LED экраны в оформлении сцены для презентации нового продукта'
+                        text4='Светодиодный экран для оформления сцены'
+                        text5='Светодиодный экран для оформления фотозоны'
+                    /> : null
+                }
+                {size.width < 1200 ?
+                    <Releases_768
+                        img1='/releases/Screen/1.jpg'
+                        img2='/releases/Screen/2.jpg'
+                        img3='/releases/Screen/3.jpg'
+                        img4='/releases/Screen/4.jpg'
+                        img5='/releases/Screen/5.jpg'
+                        text1='Угловая LED фотозона'
+                        text2='Брендированный экспостенд'
+                        text3='LED экраны в оформлении сцены для презентации нового продукта'
+                        text4='Светодиодный экран для оформления сцены'
+                        text5='Светодиодный экран для оформления фотозоны'
+                    /> : null
+                }
 
-              {/*<Seo*/}
-              {/*    image1='/Seo/Screen/1.jpg'*/}
-              {/*    image2='/Seo/Screen/2.jpg'*/}
-              {/*    text1='Банальные, но неопровержимые выводы, а также*/}
-              {/*    акционеры крупнейших компаний, которые представляют*/}
-              {/*    собой яркий пример континентально-европейского типа*/}
-              {/*     политической культуры, будут превращены в посмешище,*/}
-              {/*      хотя само их существование приносит несомненную пользу*/}
-              {/*      обществу. Высокий уровень вовлечения представителей*/}
-              {/*      целевой аудитории является четким доказательством простого*/}
-              {/*       факта: экономическая повестка сегодняшнего дня требует от*/}
-              {/*       нас анализа системы обучения кадров, соответствующей насущным*/}
-              {/*        потребностям.  экономическая повестка сегодняшнего дня требует*/}
-              {/*        от нас анализа системы*/}
-              {/*     обучения кадров, соответствующей насущным потребностям. '*/}
-              {/*    text2='Банальные, но неопровержимые выводы, а также*/}
-              {/*     акционеры крупнейших компаний, которые представляют*/}
-              {/*      собой яркий пример континентально-европейского типа*/}
-              {/*       политической культуры, будут превращены в посмешище,*/}
-              {/*        хотя само их существование приносит несомненную пользу*/}
-              {/*         обществу. Высокий уровень вовлечения представителей целевой*/}
-              {/*          аудитории является четким доказательством простого факта:*/}
-              {/*           экономическая повестка сегодняшнего дня требует от нас анализа*/}
-              {/*           системы*/}
-              {/*    обучения кадров, соответствующей насущным потребностям. '*/}
-              {/*    text3='Банальные, но неопровержимые выводы, а*/}
-              {/*     также акционеры крупнейших компаний, которые*/}
-              {/*      представляют собой яркий пример континентально-европейского*/}
-              {/*      типа политической культуры, будут превращены в посмешище, хотя*/}
-              {/*       само их существование приносит несомненную пользу обществу.*/}
-              {/*        Высокий уровень вовлечения представителей целевой аудитории*/}
-              {/*         является четким доказательством простого факта: экономическая*/}
-              {/*          повестка сегодняшнего дня требует от нас анализа системы обучения*/}
-              {/*          кадров, соответствующей насущным потребностям.  экономическая повестка*/}
-              {/*           сегодняшнего дня требует от нас анализа системы*/}
-              {/*     обучения кадров, соответствующей насущным потребностям. '*/}
-              {/*    text4=' Банальные, но неопровержимые выводы, а*/}
-              {/*    также акционеры крупнейших компаний, которые представляют*/}
-              {/*     собой яркий пример континентально-европейского типа политической*/}
-              {/*      культуры, будут превращены в посмешище, хотя само их существование*/}
-              {/*       приносит несомненную пользу обществу. Высокий уровень вовлечения*/}
-              {/*        представителей целевой аудитории является четким доказательством*/}
-              {/*         простого факта: экономическая повестка сегодняшнего дня требует*/}
-              {/*          от нас анализа системы обучения*/}
-              {/*     кадров, соответствующей насущным потребностям.'*/}
+                <Seo
+                    image1='/Seo/Screen/1.jpg'
+                    image2='/Seo/Screen/2.jpg'
+                    text1='Довольно часто на развлекательных и информационных мероприятиях используется оборудование, позволяющее транслировать информацию на большой экран. Речь идет о светодиодных экранах, которые отличаются способностью передавать изображение высокого качества с отличной яркостью. Отдельно можно отметить работоспособность оборудования.
 
-              {/*/>*/}
-              <FeedBack setOfferModal={setOfferModal} />
-          </Layout>
-          <SitesModal sites={sites} setSites= {setSites} setOfferModal={setOfferModal} title='Получить расчет'/>
-          <OfferModal offerModal={offerModal} setOfferModal={setOfferModal}/>
-          <Modal active={modalActive} setActive={setModalActive} />
-      </Provider>
+Чаще всего LED экраны используются при проведении:
+- выставок; - спортивных мероприятий, фестивалей, соревнований; - музыкальных выступлений и концертов.
+
+Довольно часто именно на LED экранах, расположенных на проходимых точках города, транслируют рекламу. Конструкцию целесообразно
+ устанавливать в местах скопления людей, транспортного потока. При таком формате эффективно размещать ролики, не превышающие по длительности пяти секунд. '
+                    text2='Не менее популярны экраны для проведения свадеб, промоакций, корпоративных встреч, презентаций нового продукта или услуги. Технология дает возможность использования не только в помещении, но и на улице практически при любых погодных условиях. Использование светодиодного экрана дает возможность привлечь внимание участников мероприятия и донести до них важную информацию.
+
+Установка светодиодного экрана позволяет настроить оптимальный уровень яркости и цвета в зависимости от условий,
+ где будет использоваться оборудование. Несомненное преимущество – низкий уровень шума и энергопотребления при работе. '
+                    text3='Оформить заказ на выгодных условиях
+
+Взять в аренду LED экран и дополнительное оборудование можно, обратившись в специализированную компанию. Специалисты не только доставят и смонтируют установку. При необходимости можно подобрать ноутбук или другие аксессуары, необходимые для работы. После завершения мероприятия экран будет демонтирован и вывезен на склад силами команды. '
+                    text4=' Чтобы обсудить условия аренды, достаточно воспользоваться формой обратной связи или позвонить по указанному на сайте телефону.'
+
+                />
+                <FeedBack setOfferModal={setOfferModal}/>
+            </Layout>
+            <SitesModal sites={sites} setSites={setSites} setOfferModal={setOfferModal} title='Получить расчет'/>
+            <OfferModal offerModal={offerModal} setOfferModal={setOfferModal}/>
+            <Modal active={modalActive} setActive={setModalActive}/>
+        </Provider>
     )
 }
 
